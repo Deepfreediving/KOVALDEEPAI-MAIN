3c812e87aeeb8dbc9037ba827fabdc9edb593b3e
deepfreediving <daniel@deepfreediving.com>
2025-07-07 00:17:36 -1000

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
       <div className="bg-white text-black p-6 rounded-2xl shadow-lg max-w-xl w-full text-center">
        <img
-  src="/logo.jpg"
+  src="/deeplogo.jpg"
   alt="Deep Freediving Logo"
   width={120}
   height={120}
